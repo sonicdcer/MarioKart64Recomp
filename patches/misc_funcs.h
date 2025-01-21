e6f521e1207cb2cef142fe3c8d2fcf2f1b679327
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2025-01-21 16:13:44 -0300

    add declarations

--- patches/misc_funcs.h
+++ patches/misc_funcs.h
@@ -15,5 +15,8 @@ DECLARE_FUNC(void, recomp_load_overlays, u32 rom, void* ram, u32 size);
 DECLARE_FUNC(s32, osPiStartDma_recomp, OSIoMesg* mb, s32 priority, s32 direction, uintptr_t devAddr, void* vAddr, size_t nbytes,
                  OSMesgQueue* mq);
 DECLARE_FUNC(float, recomp_get_aspect_ratio, float);
+DECLARE_FUNC(s32, recomp_get_target_framerate, s32);
+DECLARE_FUNC(s32, recomp_high_precision_fb_enabled);
+DECLARE_FUNC(float, recomp_get_resolution_scale);
 
 #endif
