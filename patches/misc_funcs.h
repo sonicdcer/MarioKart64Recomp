d550eb8f17b2d6adba1a3a451ffb075af786f189
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2025-01-21 16:11:38 -0300

    recomp_get_aspect_ratio declaration

--- patches/misc_funcs.h
+++ patches/misc_funcs.h
@@ -14,5 +14,6 @@ DECLARE_FUNC(s32, recomp_autosave_enabled);
 DECLARE_FUNC(void, recomp_load_overlays, u32 rom, void* ram, u32 size);
 DECLARE_FUNC(s32, osPiStartDma_recomp, OSIoMesg* mb, s32 priority, s32 direction, uintptr_t devAddr, void* vAddr, size_t nbytes,
                  OSMesgQueue* mq);
+DECLARE_FUNC(float, recomp_get_aspect_ratio, float);
 
 #endif
