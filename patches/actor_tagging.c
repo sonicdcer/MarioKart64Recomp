8d8ec82932976c8eecf941ffacc6c5aec904f9d7
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2025-01-24 04:26:08 -0300

    fix item boxes

--- patches/actor_tagging.c
+++ patches/actor_tagging.c
@@ -46,8 +46,9 @@ RECOMP_PATCH void render_actor_item_box(Camera* camera, struct ItemBox* item_box
             if (!render_set_position(someMatrix1, 0)) {
                 return;
             }
-
+            
             gSPDisplayList(gDisplayListHead++, (u8*) 0x0D002EE8);
+            
             someRot[1] = item_box->rot[1] * 2;
             someVec2[1] = item_box->pos[1];
             mtxf_pos_rotation_xyz(someMatrix1, someVec2, someRot);
@@ -56,7 +57,10 @@ RECOMP_PATCH void render_actor_item_box(Camera* camera, struct ItemBox* item_box
                 return;
             }
 
+            // Normal state
+            gDPSetTextureLUT(gDisplayListHead++, G_TT_NONE);
             gSPDisplayList(gDisplayListHead++, (u8*) 0x0d003008);
+            gDPSetTextureLUT(gDisplayListHead++, G_TT_RGBA16);
         }
         if (item_box->state == 5) {
             mtxf_pos_rotation_xyz(someMatrix1, item_box->pos, item_box->rot);
@@ -64,8 +68,9 @@ RECOMP_PATCH void render_actor_item_box(Camera* camera, struct ItemBox* item_box
             if (!render_set_position(someMatrix1, 0)) {
                 return;
             }
-
+            gDPSetTextureLUT(gDisplayListHead++, G_TT_NONE);
             gSPDisplayList(gDisplayListHead++, (u8*) 0x0d003008);
+            gDPSetTextureLUT(gDisplayListHead++, G_TT_RGBA16);
         }
         if (item_box->state != 3) {
             mtxf_pos_rotation_xyz(someMatrix1, item_box->pos, item_box->rot);
@@ -233,6 +238,7 @@ RECOMP_PATCH void render_actor_fake_item_box(Camera* camera, struct FakeItemBox*
     someRot[2] = 0;
     mtxf_pos_rotation_xyz(someMatrix2, fakeItemBox->pos, someRot);
     mtxf_scale(someMatrix2, fakeItemBox->sizeScaling);
+
     if (fakeItemBox->state != 2) {
         if (!render_set_position(someMatrix2, 0)) {
             return;
@@ -352,5 +358,103 @@ RECOMP_PATCH void render_actor_fake_item_box(Camera* camera, struct FakeItemBox*
     }
     // @recomp Pop the transform id.
     gEXPopMatrixGroup(gDisplayListHead++, G_MTX_MODELVIEW);
+}
+#endif
+
+#if 0
+RECOMP_PATCH void func_80068DA0(Player* player, UNUSED s8 arg1, UNUSED f32 arg2, s8 arg3, s8 arg4) {
+    Vec3f sp9C;
+    Vec3s sp94;
+
+    if ((player->unk_258[20 + arg4].unk_01C == 1) && ((s32) player->animFrameSelector[arg3] < 0xD)) {
+        sp9C[1] = (player->pos[1] - 3.0f) + player->unk_258[20 + arg4].unk_000[1];
+        sp9C[2] = player->pos[2] + (coss(player->unk_048[arg3]) * -10.0f);
+        sp9C[0] = player->pos[0] + (sins(player->unk_048[arg3]) * -10.0f);
+        sp94[0] = 0;
+        sp94[1] = player->unk_048[arg3];
+        sp94[2] = 0;
+        func_800652D4(sp9C, sp94, player->unk_258[20 + arg4].unk_00C * player->size);
+        gSPDisplayList(gDisplayListHead++, (Gfx*)0x0D008C90);
+        gDPLoadTLUT_pal256(gDisplayListHead++, (Gfx*)0x800E52D0);
+        gDPSetTextureLUT(gDisplayListHead++, G_TT_RGBA16);
+        gDPLoadTextureBlock(gDisplayListHead++, (Gfx*)0x8018D4A4, G_IM_FMT_CI, G_IM_SIZ_8b, 64, 32, 0,
+                            G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD,
+                            G_TX_NOLOD);
+        gSPVertex(gDisplayListHead++, (Gfx*)0x800E8B80, 4, 0);
+        gSPDisplayList(gDisplayListHead++, (Gfx*)0xd008c78);
+        gDPLoadTextureBlock(gDisplayListHead++, (Gfx*)0x8018D4A8, G_IM_FMT_CI, G_IM_SIZ_8b, 64, 32, 0,
+                            G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD,
+                            G_TX_NOLOD);
+        gSPVertex(gDisplayListHead++, (Gfx*)0x800E8BC0, 4, 0);
+        gSPDisplayList(gDisplayListHead++, (Gfx*)0x0D008DA0);
+        gDPSetTextureLUT(gDisplayListHead++, G_TT_NONE);
+        gMatrixEffectCount += 1;
+    }
+}
+#endif
+
+#if 0
+RECOMP_PATCH void func_80067964(Player* player, UNUSED s8 arg1, f32 arg2, UNUSED s8 arg3, s8 arg4) {
+    Vec3f sp9C;
+    Vec3s sp94;
+
+    if (player->unk_258[20 + arg4].unk_01C == 1) {
+        sp9C[0] = player->unk_258[20 + arg4].unk_000[0];
+        sp9C[1] = player->unk_258[20 + arg4].unk_000[1];
+        sp9C[2] = player->unk_258[20 + arg4].unk_000[2];
+        sp94[0] = 0;
+        sp94[1] = player->unk_258[20 + arg4].unk_020;
+        sp94[2] = 0;
+        func_800652D4(sp9C, sp94, player->size * arg2);
+        gSPDisplayList(gDisplayListHead++, (Gfx*)0x0D008C90);
+        gSPClearGeometryMode(gDisplayListHead++, G_CULL_BOTH);
+        gDPLoadTLUT_pal256(gDisplayListHead++, (Gfx*)0x800E52D0);
+        gDPSetTextureLUT(gDisplayListHead++, G_TT_RGBA16);
+        gDPLoadTextureBlock(gDisplayListHead++, (Gfx*)0x8018D4AC, G_IM_FMT_CI, G_IM_SIZ_8b, 64, 32, 0,
+                            G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMASK, G_TX_NOMASK,
+                            G_TX_NOLOD, G_TX_NOLOD);
+        gSPVertex(gDisplayListHead++, (Gfx*)0x800E8840, 4, 0);
+        gSPDisplayList(gDisplayListHead++, (Gfx*)0xd008c78);
+        gDPLoadTextureBlock(gDisplayListHead++, (Gfx*)0x8018D4B0, G_IM_FMT_CI, G_IM_SIZ_8b, 64, 32, 0,
+                            G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMASK, G_TX_NOMASK,
+                            G_TX_NOLOD, G_TX_NOLOD);
+        gSPVertex(gDisplayListHead++, (Gfx*)0x800E8800, 4, 0);
+        gSPDisplayList(gDisplayListHead++, (Gfx*)0x0D008DA0);
+        gDPSetTextureLUT(gDisplayListHead++, G_TT_NONE);
+        gMatrixEffectCount += 1;
+    }
+}
+#endif
+
+#if 0
+RECOMP_PATCH void func_80068310(Player* player, UNUSED s8 arg1, UNUSED f32 arg2, s8 arg3, s8 arg4) {
+    UNUSED s32 stackPadding[16]; // huh?
+    Vec3f sp9C;
+    Vec3s sp94;
+
+    if (player->unk_258[20 + arg4].unk_01C == 1) {
+        sp9C[1] = player->unk_258[20 + arg4].unk_000[1];
+        sp9C[2] = player->pos[2] + (coss(player->unk_048[arg3]) * -10.0f);
+        sp9C[0] = player->pos[0] + (sins(player->unk_048[arg3]) * -10.0f);
+        sp94[0] = 0;
+        sp94[1] = player->unk_048[arg3];
+        sp94[2] = 0;
+        func_800652D4(sp9C, sp94, player->unk_258[20 + arg4].unk_00C * player->size);
+        gSPDisplayList(gDisplayListHead++, (Gfx*)0x0D008C90);
+        gDPLoadTLUT_pal256(gDisplayListHead++, (Gfx*)0x800E52D0);
+        gDPSetTextureLUT(gDisplayListHead++, G_TT_RGBA16);
+        gDPLoadTextureBlock(gDisplayListHead++, (Gfx*)0x8018D4B4, G_IM_FMT_CI, G_IM_SIZ_8b, 64, 32, 0,
+                            G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMASK, G_TX_NOMASK,
+                            G_TX_NOLOD, G_TX_NOLOD);
+        gSPVertex(gDisplayListHead++, (Gfx*)0x800E8880, 4, 0);
+        gSPDisplayList(gDisplayListHead++, (Gfx*)0xd008c78);
+        gDPLoadTextureBlock(gDisplayListHead++, (Gfx*)0x8018D4B8, G_IM_FMT_CI, G_IM_SIZ_8b, 64, 32, 0,
+                            G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMIRROR | G_TX_CLAMP, G_TX_NOMASK, G_TX_NOMASK,
+                            G_TX_NOLOD, G_TX_NOLOD);
+        gSPVertex(gDisplayListHead++, (Gfx*)0x800E88C0, 4, 0);
+        gSPDisplayList(gDisplayListHead++, (Gfx*)0x0D008DA0);
+        gDPSetTextureLUT(gDisplayListHead++, G_TT_NONE);
+        gMatrixEffectCount += 1;
+    }
 }
 #endif
