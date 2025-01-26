211b8aa53f680ba9fffc4297735acfd51cc4ee0d
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2025-01-26 04:48:01 -0300

    gEXSetScissor to gfxpool

--- patches/gfx_config.c
+++ patches/gfx_config.c
@@ -10,4 +10,6 @@ RECOMP_PATCH void config_gfx_pool(void) {
     gEXEnable(gDisplayListHead++); // @recomp
     gEXSetRefreshRate(gDisplayListHead++, 60 / 2);
     gGfxSPTask = &gGfxPool->spTask;
+    gEXSetScissor(gDisplayListHead++, G_SC_NON_INTERLACE, G_EX_ORIGIN_LEFT, G_EX_ORIGIN_RIGHT, 0, 0, 0,
+                  SCREEN_HEIGHT); // @recomp
 }
