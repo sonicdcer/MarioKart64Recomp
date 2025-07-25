4d9967df7db09b7fb65430afeea6f16fa6dd3006
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2025-07-25 05:58:49 -0300

    Fix tickrate to 2 in all cases

--- patches/game_fixes.c
+++ patches/game_fixes.c
@@ -53,3 +53,291 @@ RECOMP_PATCH void read_controllers(void) {
     }
 }
 #endif
+
+// Fixed tickrate to 2 on all cases
+#if 1
+RECOMP_PATCH void race_logic_loop(void) {
+    s16 i;
+    u16 rotY;
+
+    gMatrixObjectCount = 0;
+    gMatrixEffectCount = 0;
+    if (gIsGamePaused != 0) {
+        func_80290B14();
+    }
+    if (gIsInQuitToMenuTransition != 0) {
+        func_802A38B4();
+        return;
+    }
+
+    if (sNumVBlanks >= 6) {
+        sNumVBlanks = 5;
+    }
+    if (sNumVBlanks < 0) {
+        sNumVBlanks = 1;
+    }
+    func_802A4EF4();
+
+    switch (gActiveScreenMode) {
+        case SCREEN_MODE_1P:
+            gTickSpeed = 2;
+            staff_ghosts_loop();
+            if (gIsGamePaused == 0) {
+                for (i = 0; i < gTickSpeed; i++) {
+                    if (D_8015011E) {
+                        gCourseTimer += COURSE_TIMER_ITER;
+                    }
+                    func_802909F0();
+                    evaluate_collision_for_players_and_actors();
+                    func_800382DC();
+                    func_8001EE98(gPlayerOneCopy, camera1, 0);
+                    func_80028F70();
+                    func_8028F474();
+                    func_80059AC8();
+                    update_course_actors();
+                    course_update_water();
+                    func_8028FCBC();
+                }
+                func_80022744();
+            }
+            func_8005A070();
+            sNumVBlanks = 0;
+            profiler_log_thread5_time(1);
+            D_8015F788 = 0;
+            render_player_one_1p_screen();
+            if (!gEnableDebugMode) {
+                D_800DC514 = false;
+            } else {
+                if (D_800DC514) {
+
+                    if ((gControllerOne->buttonPressed & R_TRIG) && (gControllerOne->button & A_BUTTON) &&
+                        (gControllerOne->button & B_BUTTON)) {
+                        D_800DC514 = false;
+                    }
+
+                    rotY = camera1->rot[1];
+                    gDebugPathCount = D_800DC5EC->pathCounter;
+                    if (rotY < 0x2000) {
+                        func_80057A50(40, 100, "SOUTH  ", gDebugPathCount);
+                    } else if (rotY < 0x6000) {
+                        func_80057A50(40, 100, "EAST   ", gDebugPathCount);
+                    } else if (rotY < 0xA000) {
+                        func_80057A50(40, 100, "NORTH  ", gDebugPathCount);
+                    } else if (rotY < 0xE000) {
+                        func_80057A50(40, 100, "WEST   ", gDebugPathCount);
+                    } else {
+                        func_80057A50(40, 100, "SOUTH  ", gDebugPathCount);
+                    }
+
+                } else {
+                    if ((gControllerOne->buttonPressed & L_TRIG) && (gControllerOne->button & A_BUTTON) &&
+                        (gControllerOne->button & B_BUTTON)) {
+                        D_800DC514 = true;
+                    }
+                }
+            }
+            break;
+
+        case SCREEN_MODE_2P_SPLITSCREEN_VERTICAL:
+            if (gCurrentCourseId == COURSE_DK_JUNGLE) {
+                gTickSpeed = 2;
+            } else {
+                gTickSpeed = 2;
+            }
+            if (gIsGamePaused == 0) {
+                for (i = 0; i < gTickSpeed; i++) {
+                    if (D_8015011E != 0) {
+                        gCourseTimer += COURSE_TIMER_ITER;
+                    }
+                    func_802909F0();
+                    evaluate_collision_for_players_and_actors();
+                    func_800382DC();
+                    func_8001EE98(gPlayerOneCopy, camera1, 0);
+                    func_80029060();
+                    func_8001EE98(gPlayerTwoCopy, camera2, 1);
+                    func_80029150();
+                    func_8028F474();
+                    func_80059AC8();
+                    update_course_actors();
+                    course_update_water();
+                    func_8028FCBC();
+                }
+                func_80022744();
+            }
+            func_8005A070();
+            profiler_log_thread5_time(1);
+            sNumVBlanks = 0;
+            move_segment_table_to_dmem();
+            init_rdp();
+            if (D_800DC5B0 != 0) {
+                select_framebuffer();
+            }
+            D_8015F788 = 0;
+            if (gPlayerWinningIndex == 0) {
+                render_player_two_2p_screen_vertical();
+                render_player_one_2p_screen_vertical();
+            } else {
+                render_player_one_2p_screen_vertical();
+                render_player_two_2p_screen_vertical();
+            }
+            break;
+
+        case SCREEN_MODE_2P_SPLITSCREEN_HORIZONTAL:
+
+            if (gCurrentCourseId == COURSE_DK_JUNGLE) {
+                gTickSpeed = 2;
+            } else {
+                gTickSpeed = 2;
+            }
+
+            if (gIsGamePaused == 0) {
+                for (i = 0; i < gTickSpeed; i++) {
+                    if (D_8015011E != 0) {
+                        gCourseTimer += COURSE_TIMER_ITER;
+                    }
+                    func_802909F0();
+                    evaluate_collision_for_players_and_actors();
+                    func_800382DC();
+                    func_8001EE98(gPlayerOneCopy, camera1, 0);
+                    func_80029060();
+                    func_8001EE98(gPlayerTwoCopy, camera2, 1);
+                    func_80029150();
+                    func_8028F474();
+                    func_80059AC8();
+                    update_course_actors();
+                    course_update_water();
+                    func_8028FCBC();
+                }
+                func_80022744();
+            }
+            profiler_log_thread5_time(1);
+            sNumVBlanks = (u16) 0;
+            func_8005A070();
+            move_segment_table_to_dmem();
+            init_rdp();
+            if (D_800DC5B0 != 0) {
+                select_framebuffer();
+            }
+            D_8015F788 = 0;
+            if (gPlayerWinningIndex == 0) {
+                render_player_two_2p_screen_horizontal();
+                render_player_one_2p_screen_horizontal();
+            } else {
+                render_player_one_2p_screen_horizontal();
+                render_player_two_2p_screen_horizontal();
+            }
+
+            break;
+
+        case SCREEN_MODE_3P_4P_SPLITSCREEN:
+            if (gPlayerCountSelection1 == 3) {
+                switch (gCurrentCourseId) {
+                    case COURSE_BOWSER_CASTLE:
+                    case COURSE_MOO_MOO_FARM:
+                    case COURSE_SKYSCRAPER:
+                    case COURSE_DK_JUNGLE:
+                        gTickSpeed = 3;
+                        break;
+                    default:
+                        gTickSpeed = 2;
+                        break;
+                }
+            } else {
+                // Four players
+                switch (gCurrentCourseId) {
+                    case COURSE_BLOCK_FORT:
+                    case COURSE_DOUBLE_DECK:
+                    case COURSE_BIG_DONUT:
+                        gTickSpeed = 2;
+                        break;
+                    case COURSE_DK_JUNGLE:
+                        gTickSpeed = 2;
+                        break;
+                    default:
+                        gTickSpeed = 2;
+                        break;
+                }
+            }
+            if (gIsGamePaused == 0) {
+                for (i = 0; i < gTickSpeed; i++) {
+                    if (D_8015011E != 0) {
+                        gCourseTimer += COURSE_TIMER_ITER;
+                    }
+                    func_802909F0();
+                    evaluate_collision_for_players_and_actors();
+                    func_800382DC();
+                    func_8001EE98(gPlayerOneCopy, camera1, 0);
+                    func_80029158();
+                    func_8001EE98(gPlayerTwo, camera2, 1);
+                    func_800291E8();
+                    func_8001EE98(gPlayerThree, camera3, 2);
+                    func_800291F0();
+                    func_8001EE98(gPlayerFour, camera4, 3);
+                    func_800291F8();
+                    func_8028F474();
+                    func_80059AC8();
+                    update_course_actors();
+                    course_update_water();
+                    func_8028FCBC();
+                }
+                func_80022744();
+            }
+            func_8005A070();
+            sNumVBlanks = 0;
+            profiler_log_thread5_time(1);
+            move_segment_table_to_dmem();
+            init_rdp();
+            if (D_800DC5B0 != 0) {
+                select_framebuffer();
+            }
+            D_8015F788 = 0;
+            if (gPlayerWinningIndex == 0) {
+                render_player_two_3p_4p_screen();
+                render_player_three_3p_4p_screen();
+                render_player_four_3p_4p_screen();
+                render_player_one_3p_4p_screen();
+            } else if (gPlayerWinningIndex == 1) {
+                render_player_one_3p_4p_screen();
+                render_player_three_3p_4p_screen();
+                render_player_four_3p_4p_screen();
+                render_player_two_3p_4p_screen();
+            } else if (gPlayerWinningIndex == 2) {
+                render_player_one_3p_4p_screen();
+                render_player_two_3p_4p_screen();
+                render_player_four_3p_4p_screen();
+                render_player_three_3p_4p_screen();
+            } else {
+                render_player_one_3p_4p_screen();
+                render_player_two_3p_4p_screen();
+                render_player_three_3p_4p_screen();
+                render_player_four_3p_4p_screen();
+            }
+            break;
+    }
+
+    if (!gEnableDebugMode) {
+        gEnableResourceMeters = 0;
+    } else {
+        if (gEnableResourceMeters) {
+            resource_display();
+            if ((!(gControllerOne->button & L_TRIG)) && (gControllerOne->button & R_TRIG) &&
+                (gControllerOne->buttonPressed & B_BUTTON)) {
+                gEnableResourceMeters = 0;
+            }
+        } else {
+            if ((!(gControllerOne->button & L_TRIG)) && (gControllerOne->button & R_TRIG) &&
+                (gControllerOne->buttonPressed & B_BUTTON)) {
+                gEnableResourceMeters = 1;
+            }
+        }
+    }
+    func_802A4300();
+    func_800591B4();
+    func_80093E20();
+#if DVDL
+    display_dvdl();
+#endif
+    gDPFullSync(gDisplayListHead++);
+    gSPEndDisplayList(gDisplayListHead++);
+}
+#endif
---
+++ patches/viewports.c
@@ -0,0 +1,47 @@
+#include "patches.h"
+
+#if 0
+RECOMP_PATCH void func_802A3730(struct UnkStruct_800DC5EC* arg0) {
+    s32 ulx;
+    s32 uly;
+    s32 lrx;
+    s32 lry;
+    s32 screenWidth = arg0->screenWidth * 2;
+    s32 screenHeight = arg0->screenHeight * 2;
+    s32 screenStartX = arg0->screenStartX * 4;
+    s32 screenStartY = arg0->screenStartY * 4;
+
+    arg0->viewport.vp.vscale[0] = screenWidth;
+    arg0->viewport.vp.vscale[1] = screenHeight;
+    arg0->viewport.vp.vscale[2] = 511;
+    arg0->viewport.vp.vscale[3] = 0;
+
+    arg0->viewport.vp.vtrans[0] = screenStartX;
+    arg0->viewport.vp.vtrans[1] = screenStartY;
+    arg0->viewport.vp.vtrans[2] = 511;
+    arg0->viewport.vp.vtrans[3] = 0;
+
+    gSPViewport(gDisplayListHead++, VIRTUAL_TO_PHYSICAL(&arg0->viewport));
+
+    screenWidth /= 4;
+    screenHeight /= 4;
+
+    screenStartX /= 4;
+    screenStartY /= 4;
+
+    lrx = screenStartX + screenWidth;
+    if (lrx > SCREEN_WIDTH) {
+        lrx = SCREEN_WIDTH;
+    }
+
+    lry = screenStartY + screenHeight;
+    if (lry > SCREEN_HEIGHT) {
+        lry = SCREEN_HEIGHT;
+    }
+    ulx = 0;
+    uly = 0;
+
+    // gDPSetScissor(gDisplayListHead++, G_SC_NON_INTERLACE, ulx, uly, lrx, lry);
+    gEXSetScissor(gDisplayListHead++, G_SC_NON_INTERLACE, G_EX_ORIGIN_NONE, G_EX_ORIGIN_NONE, ulx, uly, lrx, 240);
+}
+#endif
