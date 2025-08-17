6caea01157ec101bbfe978dce00703aeac6c41c0
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2025-08-17 16:20:53 -0300

    Bump version to 0.9.0

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -56,7 +56,7 @@
 
 #include "../../lib/rt64/src/contrib/stb/stb_image.h"
 
-const std::string version_string = "0.0.9";
+const std::string version_string = "0.9.0";
 
 template<typename... Ts>
 void exit_error(const char* str, Ts ...args) {
